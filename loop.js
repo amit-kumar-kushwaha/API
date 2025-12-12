21eac4cbc8eeee0b4d3fee2372799fd2ffdbf2c6
amt-kumar-kushwaha <amitkumarkushwaha.com.np@gmail.com>
2025-12-12 12:53:28 +0545

    loop

---
+++ loop.js
@@ -0,0 +1,9 @@
+const cars = ["BMW", "Volvo", "Saab", "Ford"];
+let len = cars.length;
+
+let text = "";
+for (let i = 0; i < len; i++) {
+  text += cars[i] + "<br>";
+}
+
+document.getElementById("demo").innerHTML = text;
